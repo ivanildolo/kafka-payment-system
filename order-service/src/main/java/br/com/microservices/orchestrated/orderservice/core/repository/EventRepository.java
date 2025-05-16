b8fcd89f04826406852d033373d2db3826398062
ivanildolo <ivanildosilvalima@gmail.com>
2025-05-16 20:51:34 -0300

    Creating the logic for receiving Saga completion notification

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
@@ -4,5 +4,4 @@ import br.com.microservices.orchestrated.orderservice.core.document.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EventRepository extends MongoRepository<Event, String> {
-
 }
