e7beb395a658cf6215092fbdc537c37ada7dfc10
ivanildolo <ivanildosilvalima@gmail.com>
2025-05-20 11:01:18 -0300

    Creating logic to make payment SUCCESSFULLY

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
@@ -4,13 +4,18 @@ package br.com.microservices.orchestrated.paymentservice.core.service;
 import br.com.microservices.orchestrated.paymentservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.paymentservice.core.PaymentRepository;
 import br.com.microservices.orchestrated.paymentservice.core.dto.Event;
+import br.com.microservices.orchestrated.paymentservice.core.dto.History;
 import br.com.microservices.orchestrated.paymentservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.paymentservice.core.enums.EPaymentStatus;
+import br.com.microservices.orchestrated.paymentservice.core.enums.ESagaStatus;
 import br.com.microservices.orchestrated.paymentservice.core.model.Payment;
 import br.com.microservices.orchestrated.paymentservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.paymentservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -19,6 +24,8 @@ public class PaymentService {
 
     private static final String CURRENT_SOURCE = "PRODUCT_";
     private static final Double REDUCE_SUM_VALUE = 0.0;
+    private static final Double MIN_AMOUNT_VALUE = 0.1;
+
 
     private final JsonUtil jsonUtil;
     private final KafkaProducer producer;
@@ -29,10 +36,19 @@ public class PaymentService {
         try {
             checkCurrentValidation(event);
             createPendingPayment(event);
+            var payment = findByOrderIdAndTransactionId(event);
+            validateAmount(payment.getTotalAmount());
+            changePaymentToSuccess(payment);
+            handleSuccess(event);
         } catch (Exception ex) {
             log.error("Error trying to make payment: ", ex);
 
         }
+    }
+
+    private void changePaymentToSuccess(Payment payment) {
+        payment.setStatus(EPaymentStatus.SUCCESS);
+        save(payment);
     }
 
     private void checkCurrentValidation(Event event) {
@@ -52,7 +68,7 @@ public class PaymentService {
                 .totalItems(totalItems)
                 .build();
         save(payment);
-        setEventAmoutItems(event, payment);
+        setEventAmountItems(event, payment);
     }
 
     private double calculateAmout(Event event){
@@ -73,9 +89,37 @@ public class PaymentService {
                 .reduce(REDUCE_SUM_VALUE.intValue(), Integer::sum);
     }
 
-    private  void setEventAmoutItems(Event event, Payment payment){
+    private  void setEventAmountItems(Event event, Payment payment){
         event.getPayload().setTotalAmount(payment.getTotalAmount());
         event.getPayload().setTotalItems(payment.getTotalItems());
+    }
+
+    private  void validateAmount(double amount){
+        if(amount < MIN_AMOUNT_VALUE){
+            throw new ValidationException("The minimum amount available is ".concat(MIN_AMOUNT_VALUE.toString()));
+        }
+    }
+
+    private void handleSuccess(Event event) {
+        event.setStatus(ESagaStatus.SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Payment realized successfully!");
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History
+                .builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+        event.addToHistory(history);
+    }
+
+    private Payment findByOrderIdAndTransactionId(Event event){
+        return paymentRepository.findByOrderIdAndTransactionId(event.getPayload().getId(), event.getTransactionId())
+                .orElseThrow(()-> new ValidationException("Payment not found by OrderId and TransactionId!"));
     }
 
     private void save(Payment payment) {
